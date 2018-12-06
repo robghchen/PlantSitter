f312ae358383172665b8d595bdfffe3f9e37c752
github username <github email address>
2018-12-06 18:22:35 -0500

    css good

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -8,7 +8,7 @@ class SessionsController < ApplicationController
     @user = User.find_by(username: params[:username])
     if @user && @user.authenticate(params[:password])
       log_in!(@user)
-      redirect_to users_path
+      redirect_to user_path(@user)
     else
       flash[:alert] = "Incorrect username/password. You suck."
       redirect_to new_session_path
--- app/models/request.rb
+++ app/models/request.rb
@@ -2,5 +2,4 @@ class Request < ApplicationRecord
   belongs_to :owner, class_name: 'User'
   belongs_to :sitter, class_name: 'User', optional: true
 
-
 end
